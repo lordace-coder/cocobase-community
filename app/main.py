e98686e357f07d8914240db7636fc34335f914a9
lordace-coder <lordyacey@gmail.com>
2025-05-27 01:32:00 +0200

    added cors support

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api import user, collections, project
 from app.core.middleware import BodySizeLimitMiddleware
 
@@ -8,7 +9,15 @@ app = FastAPI(
     description="Api docs for COCOBASE",
     docs_url="/",
     redoc_url=None,
-    
+)
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
 )
 
 # Include routers
