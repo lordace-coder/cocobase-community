40f37043a0cd362fa64785923e99a02793c83b9e
lordace-coder <lordyacey@gmail.com>
2025-10-20 08:14:47 +0200

    added migration

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,4 +3,4 @@ from .collections import Collection
 from .user import User
 from .timeline import Timeline, Suggestion, SuggestionLike
 from .cloud_functions import CloudFunction, FunctionExecution
-from .integrations import Integration, IntegrationExecution, ProjectIntegration
+from .integrations import Integration, ProjectIntegration
